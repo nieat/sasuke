140b3249c8ae1563cdcded07d287a5bc2e31d64a
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2017-08-11 16:37:10 +0900

    初期設定画面の作成・入力内容の.envファイル書き込み

--- app/controllers/config.go
+++ app/controllers/config.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"bufio"
+	"os"
+	"log"
+
 	"github.com/revel/revel"
 )
 
@@ -19,6 +23,23 @@ func (c Config) Confirm() revel.Result{
 }
 
 // 初期設定保存
-func (c Config) Save() revel.Result {
-	return  c.Render()
+// ToDo 引数は構造体（？）で定義
+func (c Config) Save(db string, host string, dbuser string, dbname string, password string) revel.Result {
+
+	// ToDo: テスト接続して接続情報の有効性を確認する。
+
+	// .envファイルへの書き込み文生成（テスト文字列）
+	dbinfoString := "db="+ db + "\n" + "host=" + host + "\n" + "dbuser=" + dbuser + "\n" + "dbname=" + dbname + "\n" + "password" + password
+
+	// .envファイルへの書き込み
+	// ToDo: 既に書き込みがある場合、一度リフレッシュする
+	write_file, _ := os.OpenFile(".env", os.O_WRONLY|os.O_APPEND, 0644)
+	bw := bufio.NewWriter(write_file)
+	_, err := bw.WriteString(dbinfoString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bw.Flush()
+
+	return c.Redirect(App.Index) //ToDo: トップページにリダイレクト
 }
