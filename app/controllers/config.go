34494ba33e4fe9114eb2e10434db637720817c85
kousuke.tanihata <kousuke.econ15060709@hotmail.co.jp>
2017-08-11 13:02:47 +0900

    fix config

--- app/controllers/config.go
+++ app/controllers/config.go
@@ -2,21 +2,21 @@ package controllers
 
 import ("github.com/revel/revel")
 
-type App struct {
+type Config struct {
 	*revel.Controller
 }
 
 // 初期設定入力ページ
-func (c App) Index() revel.Result {
+func (c Config) Index() revel.Result {
 	return c.Render()
 }
 
 // 初期設定確認ページ
-func (c App) Confirm() revel.Result{
+func (c Config) Confirm() revel.Result{
 	return c.Render()
 }
 
 // 初期設定保存
-func (c App) Save() revel.Result {
+func (c Config) Save() revel.Result {
 	return  c.Render()
 }
