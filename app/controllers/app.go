0b418020ddb270f00f56c2d310c9c6414dfb5bdd
kousuke.tanihata <kousuke.econ15060709@hotmail.co.jp>
2017-08-13 12:51:07 +0900

    一旦コミット

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,8 +11,32 @@ type App struct {
 
 func (c App) Index() revel.Result {
 	s := &sql.Handler{}
-	s.FetchTableSchema()
-	return c.Render()
+	table_name_slice, column_name_slice := s.FetchTableSchema()
+	// tableのダブり値を削除
+	m := make(map[string]bool)
+	uniq := [] string{}
+
+	for _, ele := range table_name_slice {
+		if !m[ele] {
+			m[ele] = true
+			uniq = append(uniq, ele)
+		}
+	}
+
+	// var table_schema [ len(uniq) ][len(column_name_slice)]string
+
+	//for i:=0 ;i < len(column_name_slice) ;i++  {
+	//
+	//	fmt.Printf(table_name_slice[i])
+	//	fmt.Printf(column_name_slice[i])
+	//	before_index := i -1
+	//	// テーブル名変更された時
+	//	if table_name_slice[i] != table_name_slice[before_index] {
+	//		table_schema[i][0] = table_name_slice[i]
+	//		table_schema[i] = append(table_schema[i], column_name_slice[i])
+	//	}
+	//}
+	return c.Render(table_name_slice, column_name_slice)
 }
 
 func (c App) Execute() revel.Result {
--- app/service/sql/sql.go
+++ app/service/sql/sql.go
@@ -1,11 +1,8 @@
 package sql
 
 import (
-	//"github.com/gocraft/dbr"
 	_ "github.com/go-sql-driver/mysql"
 	//"github.com/joho/godotenv"
-	//"log"
-	"fmt"
 	"log"
 	"database/sql"
 )
@@ -13,16 +10,8 @@ import (
 type Handler struct {
 }
 
-//var (
-//	db			string
-//	host		string
-//	dbuser		string
-//	dbname		string
-//	password	string
-//	port        string
-//)
+func (h *Handler)FetchTableSchema ()([]string,[]string){
 
-func (h *Handler)FetchTableSchema () {
 	//loadEnv();
 	db := "mysql"
 	host :="localhost"
@@ -31,106 +20,39 @@ func (h *Handler)FetchTableSchema () {
 	dbname := "SASUKE_TEST"
 	password := "password"
 
-	//conn, err := dbr.Open(db,dbuser+":"+password+"@tcp("+host+":"+port+")/use information_schema",nil)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//session := conn.NewSession(nil)
-	//var tables []string
-	//session.SelectBySql("show tables").Load(&tables)
-
-	//var table_schema [][]string
 	mysql, error := sql.Open(db,dbuser+":"+password+"@tcp("+host+":"+port+")/"+"information_schema")
 	if error != nil {
 		log.Fatal("open erro: %v", error)
 	}
 
-		//use_query := "use information_schema;"
-		main_query := "select table_name,column_name from columns where table_schema=' "+dbname+ `';`
-
-		//mysql.Query(use_query)
+		main_query :=`select table_name,column_name from columns where table_schema="`+dbname+ `";`
 		rows, err := mysql.Query(main_query)
 
 	if err != nil {
 		panic(err.Error())
 	}
+	defer rows.Close()
 
-	columns, err := rows.Columns() // カラム名を取得
-	if err != nil {
-		panic(err.Error())
+	type TableSchema struct {
+		table_name string
+		column_name string
 	}
 
-	values := make([]sql.RawBytes, len(columns))
-	fmt.Print(len(values))
-	//  rows.Scan は引数に `[]interface{}`が必要.
-
-	scanArgs := make([]interface{}, len(values))
-	for i := range values {
-		scanArgs[i] = &values[i]
-	}
+	var table_name_slice []string
+	var column_name_slice []string
 
 	for rows.Next() {
-		err = rows.Scan(scanArgs...)
-		if err != nil {
-			panic(err.Error())
+		var table_name string
+		var column_name string
+		if err := rows.Scan(&table_name, &column_name); err != nil {
+			log.Fatal(err)
 		}
 
-		var value string
-		for i, col := range values {
-			// Here we can check if the value is nil (NULL value)
-			if col == nil {
-				value = "NULL"
-			} else {
-				value = string(col)
-			}
-			fmt.Println(columns[i], ": ", value)
-		}
-		fmt.Println("-----------------------------------")
+		table_name_slice = append(table_name_slice,table_name)
+		column_name_slice = append(column_name_slice,column_name)
 	}
-		//defer rows.Close()
-		//
-		//if err != nil {
-		//	log.Fatal( err)
-		//}
-		//
-		//columns, err := rows.Columns()
-		//fmt.Print(columns)
-		//
-		//values := make([]sql.RawBytes, len(columns))
-		//scanArgs := make([]interface{}, len(values))
-		//
-		//for i := range values{
-		//	scanArgs[i] = &values[i]
-		//}
-		//
-		//for rows.Next() {
-		//	err = rows.Scan(scanArgs...)
-		//	if err != nil{
-		//		panic(err.Error())
-		//	}
-		//
-		//	var value string
-		//	for _, col := range values{
-		//		if col == nil{
-		//			value = "NULL"
-		//		}else{
-		//			value = string(col)
-		//		}
-		//		fmt.Println(value)
-		//	}
-		//}
-		//fmt.Println("---------------")
 
-
-
-	//fmt.Println(table_columns[:]);
-
-
-	// 内容を整形する
-
-
-
-	return
+	return table_name_slice,column_name_slice
 }
 
 
